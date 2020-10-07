439bbe83c3633488b2d1b923c4f55709fb60c078
priyanka <priyanka.kumari@tathastu.ai>
2020-10-08 02:02:04 +0530

    Updates

---
+++ precache-manifest.c55dc54119cd42370d2765d6c0486392.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2553c990cdc80a24a223dd2b080f9ff6",
+    "url": "/react-icon-card-listings/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/react-icon-card-listings/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aeca2a4acea35d86bc84",
+    "url": "/react-icon-card-listings/static/css/main.91b1c45c.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/react-icon-card-listings/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-icon-card-listings/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeca2a4acea35d86bc84",
+    "url": "/react-icon-card-listings/static/js/main.a27bee67.chunk.js"
+  },
+  {
+    "revision": "73779a62f1454d3f5d09",
+    "url": "/react-icon-card-listings/static/js/runtime-main.06e158a9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-icon-card-listings/static/media/SEGA.12a3db8b.woff"
+  }
+]);
